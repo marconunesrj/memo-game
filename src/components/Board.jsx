01db6a5603b3df1953f83cd6af8a30da5579452c
MARCONUNES\marco <marconunes.rj@hotmail.com>
2026-02-04 12:13:47 -0300

    Primeiros componentes da aplicação

---
+++ src/components/Board.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from './Card'
+
+
+export function Board() {    
+    return (
+        <div>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function Card() {
+  return <button>Card Component</button>
+}
